27211db7cd5931be4758a9e93f0d6560cc008f78
minjeong073 <kim.mj80730@gmail.com>
2023-05-12 23:31:49 +0900

    [0509] useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 const App = () => {
   const [row, setRow] = useState([]);
 
-  const onClickButton = () => {
+  // useEffect 활용 방법
+  // 1번째
+  useEffect(() => {
+    console.log("mount or update");
+
+    // 내용이 지워질 때 (unmount 될 때) 실행하기 전에 실행되는
+    return () => {
+      console.log("unmount");
+    };
+  });
+
+  // 2번째 : mount
+  // 빈 배열 던짐
+  useEffect(() => {
+    console.log("mount only");
+  }, []);
+
+  // 3번째 :
+  useEffect(() => {
+    console.log("update only", row);
+  }, [row]); // row 가 update 됐을 때만 해당 함수 실행
+
+  useEffect(() => {
     if (row.length == 0) {
       fetch(
         "http://openapi.seoul.go.kr:8088/6e747a6b44616c733732614e53414f/json/RealtimeCityAir/1/25"
@@ -14,13 +37,11 @@ const App = () => {
         });
       });
     }
-  };
+  }, []);
 
   return (
     <>
-      <button onClick={onClickButton} className="loadingButton">
-        loading
-      </button>
+      <button className="loadingButton">loading</button>
       <table className="table">
         <thead>
           <tr>
