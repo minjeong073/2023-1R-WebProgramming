278e40f74d988665b7b560b8597e476e54c3499b
minjeong073 <kim.mj80730@gmail.com>
2023-05-13 01:06:39 +0900

    [0511] worldcup

---
+++ src/Worldcup.jsx
@@ -0,0 +1,116 @@
+import { useEffect, useState } from "react";
+import "./worldcup.css";
+
+import a01 from "./assets/HowlsMovingCastle.jpg";
+import a02 from "./assets/BossBaby.jpg";
+import a03 from "./assets/HowToTrainYourDragon.jpg";
+import a04 from "./assets/MyNeighborTotoro.jpg";
+import a05 from "./assets/WallaceAndGromit.jpg";
+import a06 from "./assets/DespicableMe.jpg";
+import a07 from "./assets/WallE.jpg";
+import a08 from "./assets/SpiritedAway.jpg";
+import a09 from "./assets/Coco.jpg";
+import a10 from "./assets/Frozen.jpg";
+import a11 from "./assets/MonstersINC.jpg";
+import a12 from "./assets/CorpseBride.jpg";
+import a13 from "./assets/Incredibles.jpg";
+import a14 from "./assets/CharlieAndTheChocolateFactory.jpg";
+import a15 from "./assets/FindingNEMO.jpg";
+import a16 from "./assets/YourName.jpg";
+import versus from "./assets/versus.png";
+
+function Worldcup() {
+  const candidate = [
+    { name: "하울의 움직이는 성", src: a01 },
+    { name: "보스베이비", src: a02 },
+    { name: "드래곤 길들이기", src: a03 },
+    { name: "이웃집 토토로", src: a04 },
+    { name: "월레스와 그로밋", src: a05 },
+    { name: "슈퍼배드", src: a06 },
+    { name: "월 E", src: a07 },
+    { name: "센과 치히로의 행방불명", src: a08 },
+    { name: "코코", src: a09 },
+    { name: "겨울왕국", src: a10 },
+    { name: "몬스터 주식회사", src: a11 },
+    { name: "유령신부", src: a12 },
+    { name: "인크레더블", src: a13 },
+    { name: "찰리와 초콜릿 공장", src: a14 },
+    { name: "니모를 찾아서", src: a15 },
+    { name: "너의 이름은", src: a16 },
+  ];
+
+  const [game, setGame] = useState([]);
+  const [round, setRound] = useState(0);
+  const [nextGame, setNextGame] = useState([]);
+
+  useEffect(() => {
+    setGame(
+      candidate
+        .map((c) => {
+          return { name: c.name, src: c.src, order: Math.random() };
+        })
+        .sort((l, r) => {
+          return l.order - r.order;
+        })
+    );
+  }, []);
+
+  useEffect(() => {
+    if (game.length > 1 && round + 1 > game.length / 2) {
+      setGame(nextGame);
+      setNextGame([]);
+      setRound(0);
+    }
+  }, [round]);
+
+  if (game.length === 1)
+    return (
+      <div className="wrapper">
+        <div className="header_title">영화 월드컵 우승!</div>
+        <div>
+          <img src={game[0].src} className="movie_image" />
+          <p className="movie_name">{game[0].name}</p>
+        </div>
+      </div>
+    );
+
+  if (game.length === 0 || round + 1 > game.length / 2)
+    return <p>로딩중입니다</p>;
+
+  return (
+    <div className="wrapper">
+      <div className="header_title">
+        애니메이션 영화 월드컵 {round + 1} / {game.length / 2}{" "}
+        <b>{game.length === 2 ? "결승" : game.length + "강"}</b>
+        <p className="subtitle">가장 감명 깊게 본 영화를 선택하세요!</p>
+      </div>
+      <div>
+        <img
+          src={game[round * 2].src}
+          className="movie_image"
+          onClick={() => {
+            setNextGame((prev) => prev.concat(game[round * 2]));
+            setRound((r) => r + 1);
+          }}
+        />
+        <p className="movie_name">{game[round * 2].name}</p>
+      </div>
+      <div>
+        <img
+          src={game[round * 2 + 1].src}
+          className="movie_image"
+          onClick={() => {
+            setNextGame((prev) => prev.concat(game[round * 2 + 1]));
+            setRound((r) => r + 1);
+          }}
+        />
+        <p className="movie_name">{game[round * 2 + 1].name}</p>
+      </div>
+      <img src={versus} className="versus_image" />
+    </div>
+  );
+}
+
+export default Worldcup;
+
+// round 바뀔 때마다 check
